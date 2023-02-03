84edacdd9bf8a95e2a1ba6cc77cb50f07f0266d4
Henry Grantham-Smith <116105522+HenryGSmith@users.noreply.github.com>
2023-02-03 06:37:24 +0000

    updated input to batch

--- Program.cs
+++ Program.cs
@@ -8,14 +8,19 @@ namespace NeuralNetworkFromScratch
 	{
 		static void Main(string[] args)
 		{
-			Vector<double> inputs = DenseVector.OfArray(new double[]{ 1, 2, 3, 2.5 });
+			Matrix<double> batch = DenseMatrix.OfArray(new double[,]
+								{{  1.00,  2.00,  3.00,  2.50 },
+								 {  2.00,  5.00, -1.00,  2.00 },
+								 { -1.50,  2.70,  3.30, -0.80 }});
+
 			Matrix<double> weights = DenseMatrix.OfArray(new double[,]
 								{{  0.20,  0.80, -0.50,  1.00 }, 
 								 {  0.50, -0.91,  0.26, -0.50 }, 
 								 { -0.26, -0.27,  0.17,  0.87 }});
-			Vector<double> biases = DenseVector.OfArray(new double[]{ 2, 3, 0.5 });
+			Matrix<double> biases = DenseMatrix.OfArray(new double[,]
+								{{ 2, 3, 0.5 }, { 2, 3, 0.5 }, { 2, 3, 0.5 } });
 
-			Vector<double> outputs = (weights * inputs) + biases;
+			Matrix<double> outputs = (batch * weights.Transpose()) + biases;
 
 			Console.WriteLine(outputs.ToString());
 		}
