c614f270d9fbb9654ef8d72bc07e319839baa2ff
Henry Grantham-Smith <116105522+HenryGSmith@users.noreply.github.com>
2023-02-03 05:12:48 +0000

    update to utils

--- Program.cs
+++ Program.cs
@@ -5,16 +5,14 @@
 		static void Main(string[] args)
 		{
 			double[] inputs = { 1, 2, 3, 2.5 };
-			double[] weights1 = { 0.2, 0.8, -0.5, 1.0 };
-			double[] weights2 = { 0.5, -0.91, 0.26, -0.5 };
-			double[] weights3 = { -0.26, -0.27, 0.17, 0.87 };
-			double bias1 = 2;
-			double bias2 = 3;
-			double bias3 = 0.5;
+			double[,] weights = {{  0.20,  0.80, -0.50,  1.00 }, 
+								 {  0.50, -0.91,  0.26, -0.50 }, 
+								 { -0.26, -0.27,  0.17,  0.87 }};
+			double[] biases = { 2, 3, 0.5 };
 
-			TestNeurone neurone1 = new TestNeurone(inputs, weights1, bias1);
-			TestNeurone neurone2 = new TestNeurone(inputs, weights2, bias2);
-			TestNeurone neurone3 = new TestNeurone(inputs, weights3, bias3);
+			TestNeurone neurone1 = new TestNeurone(inputs, weights, biases[0]);
+			TestNeurone neurone2 = new TestNeurone(inputs, weights, biases[1]);
+			TestNeurone neurone3 = new TestNeurone(inputs, weights, biases[2]);
 
 
 			double[] output = { neurone1.Output(), neurone2.Output(), neurone3.Output() };
