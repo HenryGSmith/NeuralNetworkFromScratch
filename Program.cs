2a587ccd19a8a0e3eb77cb35dace4a711f05db70
Henry Grantham-Smith <116105522+HenryGSmith@users.noreply.github.com>
2023-02-06 14:57:51 +0000

    testing ReLU activation

--- Program.cs
+++ Program.cs
@@ -15,13 +15,29 @@ namespace NeuralNetworkFromScratch
 								 { -1.50,  2.70,  3.30, -0.80 }});
 
 
+			double[] inputs = { 0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100 };
+			double[] outputs = new double[inputs.Length];
+
+			// ReLU activation
+			for (int i = 0; i < inputs.Length; i++)
+			{
+				if (inputs[i] > 0)
+				{
+					outputs[i] = inputs[i];
+				}
+				else
+				{
+					outputs[i] = 0;
+				}
+			}
+
+			/*
 			LayerDense l1 = new LayerDense(4,5);
 			LayerDense l2 = new LayerDense(5,2);
 			l1.Forward(X);
 			l2.Forward(l1.outputs);
 
-			//Console.WriteLine(l1.outputs.ToString());
-			Console.WriteLine(l2.outputs.ToString());
+			Console.WriteLine(l2.outputs.ToString());*/
 		}
 	}
 }
