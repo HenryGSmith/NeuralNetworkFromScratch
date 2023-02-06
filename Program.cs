a568bb17d25ab4583ad89f663a92e358818b8193
Henry Grantham-Smith <116105522+HenryGSmith@users.noreply.github.com>
2023-02-06 15:26:07 +0000

    added temporary dataset

--- Program.cs
+++ Program.cs
@@ -29,5 +29,29 @@ namespace NeuralNetworkFromScratch
 
 			Console.WriteLine(l2.Outputs.ToString());*/
 		}
+
+		// standin dataset
+		Matrix<double> spiral_data(int points, int classes)
+		{
+			//Matrix<double> X;
+			//Vector<double> y;
+			var M = Matrix<double>.Build; //shortcut to Matrix builder
+			var V = Vector<double>.Build; //shortcut to Vector builder
+
+			//build vectors of size points*classesx1 for y, r and theta
+			var y = V.Dense(points * classes); //at this point this is full of zeros
+			for (int j = 0; j < classes; j++)
+			{
+				var y_step = V.DenseOfArray(Generate.Step(points * classes, 1, (j + 1) * points));
+				y = y + y_step;
+			}
+			var r = V.DenseOfArray(Generate.Sawtooth(points * classes, points, 0, 1));
+			var theta = 4 * (r + y) + (V.DenseOfArray(Generate.Standard(points * classes)) * 0.2);
+			var sin_theta = theta.PointwiseSin();
+			var cos_theta = theta.PointwiseCos();
+
+			var X = M.DenseOfColumnVectors(r.PointwiseMultiply(sin_theta), r.PointwiseMultiply(cos_theta));
+			return X;
+		}
 	}
 }
