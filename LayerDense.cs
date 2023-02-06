40f1c888c428eab15c1e4ff17ec67789187494fc
Henry Grantham-Smith <116105522+HenryGSmith@users.noreply.github.com>
2023-02-06 14:41:27 +0000

    upsdated layer logic

---
+++ LayerDense.cs
@@ -0,0 +1,31 @@
+﻿using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace NeuralNetworkFromScratch
+{
+	internal class LayerDense
+	{
+		Matrix<double> weights;
+		double[] biases;
+		int nNeurons;
+
+		public Matrix<double> outputs { get; private set; }
+
+		MatrixBuilder<double> M = Matrix<double>.Build;
+
+		public LayerDense(int nInputs, int nNeurons) 
+		{
+			weights = M.Random(nInputs, nNeurons) * 0.10;
+			biases = new double[nNeurons];
+			this.nNeurons = nNeurons;
+		}
+
+		public Matrix<double> Forward(Matrix<double> inputs)
+		{
+			Matrix<double> bias = M.Dense(inputs.RowCount, nNeurons, (i, j) => biases[j]);
+			outputs = (inputs * weights) + bias;
+			return outputs;
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -8,21 +8,20 @@ namespace NeuralNetworkFromScratch
 	{
 		static void Main(string[] args)
 		{
-			Matrix<double> batch = DenseMatrix.OfArray(new double[,]
+			// training data set
+			Matrix<double> X = DenseMatrix.OfArray(new double[,]
 								{{  1.00,  2.00,  3.00,  2.50 },
 								 {  2.00,  5.00, -1.00,  2.00 },
 								 { -1.50,  2.70,  3.30, -0.80 }});
 
-			Matrix<double> weights = DenseMatrix.OfArray(new double[,]
-								{{  0.20,  0.80, -0.50,  1.00 }, 
-								 {  0.50, -0.91,  0.26, -0.50 }, 
-								 { -0.26, -0.27,  0.17,  0.87 }});
-			Matrix<double> biases = DenseMatrix.OfArray(new double[,]
-								{{ 2, 3, 0.5 }, { 2, 3, 0.5 }, { 2, 3, 0.5 } });
 
-			Matrix<double> outputs = (batch * weights.Transpose()) + biases;
+			LayerDense l1 = new LayerDense(4,5);
+			LayerDense l2 = new LayerDense(5,2);
+			l1.Forward(X);
+			l2.Forward(l1.outputs);
 
-			Console.WriteLine(outputs.ToString());
+			//Console.WriteLine(l1.outputs.ToString());
+			Console.WriteLine(l2.outputs.ToString());
 		}
 	}
 }
